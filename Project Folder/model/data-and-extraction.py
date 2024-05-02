6a49eb8068a7c9a6b4d4a93c244099f717ae6622
Shaaz Charania <shaazch@uw.edu>
2024-05-01 22:20:18 -0700

    added code fo rfeature extraction

--- Project Folder/model/data-and-extraction.py
+++ Project Folder/model/data-and-extraction.py
@@ -1,8 +1,11 @@
 import librosa
 import numpy as np
 
+
 def extract_features(audio_path):
     y, sr = librosa.load(audio_path, sr=None)
     S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
     log_S = librosa.power_to_db(S, ref=np.max)
     return log_S.flatten()
+
+
